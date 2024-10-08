87ccff408cc4e48b45504a6b70f3f479c4d37579
tuhin93 <tuhin93.khan@gmail.com>
2024-10-08 15:48:01 +0600

    added home page and validte login button

--- js/login.js
+++ js/login.js
@@ -1,6 +1,25 @@
 
-document.getElementById('btn-login')
-.addEventListener('click', function (event){
+// document.getElementById('btn-login')
+//     .addEventListener('click', function (event) {
+//         event.preventDefault();
+//         const phoneNumber = document.getElementById('phone-number').value;
+//         const pinNumber = document.getElementById('pin-number').value;
+//         // console.log('Your phone number is', phoneNumber, 'and your pin number is', pinNumber);
+//         if(phoneNumber === '01825555893' && pinNumber === '1111'){
+//             console.log('You are logged in');
+//         }
+//         else{
+//             alert('Wrong number or password')
+//         }
+//     })
+document.getElementById('btn-login').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('button clicking file added');
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    if (phoneNumber === '017' && pinNumber === '0000') {
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('You entered wrong info');
+    }
 })
